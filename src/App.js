1d0edd590e725803c8c79d70aab12f7ebf20f088
Tempesto <igor.it.step@gmail.com>
2018-11-09 16:40:02 +0200

    'addMembers'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React, { Component } from 'react'
-import '../src/style/style.css'
+import React, { Component } from "react";
+import "../src/style/style.css";
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<h1>My app</h1>
-			</div>
-		)
-	}
+  state = {
+    members: [
+      {
+        id: "0",
+        telefon: "+380991",
+        name: "Ross",
+        company: "Ross Company",
+        email: "ross@mail.com",
+        foto: "fotoRoss"
+      },
+      {
+        id: "1",
+        telefon: "+380992",
+        name: "Terry",
+        company: "Terry Company",
+        email: "terry@mail.com",
+        foto: "fotoTerry"
+      },
+      {
+        id: "2",
+        telefon: "+380993",
+        name: "Abby",
+        company: "Abby Company",
+        email: "abby@mail.com",
+        foto: "fotoAbby"
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="App">
+        {this.state.members.map(elem => {
+          return (
+            <div className="contact">
+              <div key={elem.id}>
+                <div>Name: {elem.name}</div>
+                <div>Telefon: {elem.telefon}</div>
+                <div>Company: {elem.company}</div>
+                <div>Email: {elem.email}</div>
+                <div>Foto: {elem.foto}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
